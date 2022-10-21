ca1e667958a60fb1319a1755219f173a0d17e0a3
Monte Hoover <monte.b.hoover@gmail.com>
2022-10-21 15:16:59 -0400

    Add symlink to help with VSCode powershell extension.

--- windows/update_profile.ps1
+++ windows/update_profile.ps1
@@ -1,4 +1,7 @@
 echo "Attempting to create symlink for powershell profile..."
 rm %UserProfile%\OneDrive\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+# create a symlink for the normal user profile
 gsudo cmd /c mklink %UserProfile%\OneDrive\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 %UserProfile%\.dotfiles\windows\profile.ps1
+# create a symlink for a weird profile that I got one time from VS Code's powershell extension
+gsudo cmd /c mklink %UserProfile%\OneDrive\Documents\WindowsPowerShell\Microsoft.VSCode_profile.ps1 %UserProfile%\.dotfiles\windows\profile.ps1
 echo "Completed successfully."
