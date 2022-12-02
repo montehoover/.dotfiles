a376bd6e01117a5542a0f7788d75faa8fea6f1cc
Monte Hoover <monte.b.hoover@gmail.com>
2022-12-02 16:55:38 -0500

    Add ll function that shows hidden files.

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -1,3 +1,7 @@
+# This file is on disk at $HOME\.dotfiles\windows\profile.ps1
+# You might be seeing it symlinked at $HOME\OneDrive\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 or $HOME\\OneDrive\Documents\WindowsPowerShell\Microsoft.VSCode_profile.ps1
+# Since it is symlinked, feel free to edit this file wherever you find it, but be sure to commit any useful changes in the .dotfiles git repo.
+
 echo "Running $profile"
 
 # Choose oh-my-posh theme
@@ -27,20 +31,24 @@ Import-Module Z
 
 # ssh-copy-id equivalent
 Import-Module $HOME/.dotfiles/windows/Copy-SSHKey.ps1
+Set-Alias -Name ssh-copy-id -Value Copy-SSHKey
 
 # linux-like aliases
 Set-Alias -Name grep  -Value findstr
-Set-Alias -Name ll    -Value ls
 Set-Alias -Name open  -Value start
 Set-Alias -Name sudo  -Value gsudo
 Set-Alias -Name less  -Value more
 Set-Alias -Name zip   -Value Compress-Archive
 Set-Alias -Name unzip -Value Expand-Archive
 
+# More linux-like aliases, but powershell aliases can only be single words (even with quotes) so use functions instead:
 function which {Get-Command -All $args}
+function ll {Get-ChildItem -Force $args}
 
 # Function so I can "rm -rf" some folder
+# First remove the alias if it already exists
 if (Get-alias rm -ErrorAction SilentlyContinue) {Remove-Item alias:rm}
+# Notice that "rf" is treated as a single argument, so this won't work with "rm -f" or "rm -r".
 function rm ([switch]$rf, $item) {
   if ($rf) {
     Remove-Item $item -Recurse -Force
